84b0113530b0f24a50c9d92f1dc71eb691286f39
NotHolmes <zahil.c@ku.th>
2024-03-07 21:09:05 +0700

    re-structure src files

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@ set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
 
 echo "Starting server"
-python server.py &
+python src/server.py &
 sleep 3  # Sleep for 3s to give the server enough time to start
 
 for i in $(seq 0 1); do
     echo "Starting client $i"
-    python client.py --client-id "${i}" &
+    python src/client.py --client-id "${i}" &
 done
 
 # This will allow you to use CTRL+C to stop all background processes
