484ca032f402f71e4ab5d83dc88d96acb45b2089
NotHolmes <zahil.c@ku.th>
2024-03-07 18:21:59 +0700

    add load_dataset()

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-
-from flwr.common import NDArrays
+import pandas as pd
+from flwr.common import 
+from sklearn.model_selection import train_test_split
 
 
 def get_model_parameters(model: LogisticRegression) -> NDArrays:
@@ -33,10 +34,22 @@ def set_initial_params(model: LogisticRegression):
     But server asks for initial parameters from clients at launch. Refer to
     sklearn.linear_model.LogisticRegression documentation for more information.
     """
-    n_classes = 10  # MNIST has 10 classes
-    n_features = 784  # Number of features in dataset
+    n_classes = 3  # has 3 classes
+    n_features = 54  # Number of features in dataset
     model.classes_ = np.array([i for i in range(10)])
 
     model.coef_ = np.zeros((n_classes, n_features))
     if model.fit_intercept:
         model.intercept_ = np.zeros((n_classes,))
+
+def load_dataset(dataset_path: str):
+    """Loads the dataset from the given path."""
+    
+    df = pd.read_csv(dataset_path)
+    
+    X = df.drop('Credit_Score', axis=1)
+    y = df['Credit_Score']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    return X_train, X_test, y_train, y_test
