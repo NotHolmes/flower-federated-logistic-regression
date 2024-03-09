27201679a453d0ffcea72ac32014b20d80611f49
NotHolmes <zahil.c@ku.th>
2024-03-09 18:02:52 +0700

    change run.sh to 3 clients

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ else
     sleep 3  # Sleep for 3s to give the server enough time to start
 fi
 
-for i in $(seq 0 1 4); do
+for i in $(seq 0 1 2); do
     echo "Starting client $i"
     python src/client.py --client-id "${i}" &
 done
