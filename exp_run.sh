6f670ec338a30ab8ae23bf75c1ac3c8340fcbff7
NotHolmes <zahil.c@ku.th>
2024-03-10 17:49:51 +0700

    add result and exp.sh

---
+++ exp_run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
+
+# Check if the loop count is provided as a command-line argument
+if [[ $# -eq 0 ]]; then
+    echo "Please provide the loop count as a command-line argument."
+    exit 1
+fi
+
+# Store the loop count in a variable
+loop_count=$1
+
+# Function to run the script
+run_script() {
+    if [[ "$1" == "--client-only" || "$1" == "-cl" ]]; then
+        :  # Do nothing if client-only mode is specified
+    elif [[ "$1" == "--trim" || "$1" == "-t" ]]; then
+        echo "Starting FedTrimmedAvg server"
+        python src/server_fedtrimmedavg.py &
+        sleep 3  # Sleep for 3s to give the server enough time to start
+    elif [[ "$1" == "--med" || "$1" == "-m" ]]; then
+        echo "Starting FedMedian server"
+        python src/server_fedmedian.py &
+        sleep 3  # Sleep for 3s to give the server enough time to start
+    elif [[ "$1" == "--krum" || "$1" == "-k" ]]; then
+        echo "Starting Krum server"
+        python src/server_krum.py &
+        sleep 3  # Sleep for 3s to give the server enough time to start
+    else
+        echo "Starting default (FedAvg) server"
+        python src/server.py &
+        sleep 3  # Sleep for 3s to give the server enough time to start
+    fi
+
+    for i in $(seq 0 1 2); do
+        echo "Starting client $i"
+        python src/client.py --client-id "${i}" &
+    done
+
+    # This will allow you to use CTRL+C to stop all background processes
+    trap 'trap - SIGTERM && kill -- -$$' SIGINT SIGTERM
+    # Wait for all background processes to complete
+    wait
+
+    # Run python src/matrices.py after server and client are done
+    python src/matrices.py --model "$1" >> "$1_result.txt"
+}
+
+# Loop the script based on the provided loop count
+for ((count=1; count<=$loop_count; count++)); do
+    echo "Loop $count"
+    run_script "$@"
+done
---
+++ src/matrices.py
@@ -0,0 +1,60 @@
+import argparse
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+import utils
+from joblib import load
+import numpy as np
+import seaborn as sns
+
+from sklearn.metrics import (
+    accuracy_score, f1_score, precision_score, recall_score, confusion_matrix, classification_report, ConfusionMatrixDisplay
+)
+import matplotlib.pyplot as plt
+pd.set_option('display.max_columns', 30)
+plt.style.use('ggplot')
+
+def generate_report(y_test, y_pred, title):
+    acc_score = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average=None)
+    precision = precision_score(y_test, y_pred, average=None)
+    recall = recall_score(y_test, y_pred, average=None)
+
+    print(f"{title}")
+    print(f"Accuracy: {acc_score:.2%}\n\
+    F1 Score: {dict(zip([0, 1, 2], map(lambda x: f'{x:.2%}', f1)))}\n\
+    Precision Score: {dict(zip([0, 1, 2], map(lambda x: f'{x:.2%}', precision)))}\n\
+    Recall Score: {dict(zip([0, 1, 2], map(lambda x: f'{x:.2%}', recall)))}\n\
+    ")
+
+def main(model_type):
+    X_test, y_test = utils.load_dataset("dataset/test.csv")
+    X_test, _ = utils.scale_data(X_test, X_test)
+
+    # shuffle the data
+    X_test = X_test.sample(frac=1)
+    y_test = y_test.sample(frac=1)
+
+    if model_type == "avg":
+        model = load('model/model_fedavg.joblib')
+    elif model_type == "trim":
+        model = load('model/model_fedtrimmedavg.joblib')
+    elif model_type == "krum":
+        model = load('model/model_krum.joblib')
+    else:
+        print("Invalid model type.")
+        return
+
+    y_pred = model.predict(X_test)
+    generate_report(y_test, y_pred, model_type.capitalize())
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", help="Specify the model type: avg, trim, or krum")
+    args = parser.parse_args()
+
+    if args.model:
+        main(args.model)
+    else:
+        main("avg")
